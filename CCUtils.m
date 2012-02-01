67052d9beedadcf950f055c538abd96685cd53f9
Andreas Ganske <a.ganske@chaosspace.de>
2012-02-02 00:06:06 +0100

    Added author name

--- CCUtils.m
+++ CCUtils.m
@@ -3,7 +3,7 @@
 //  CCLibrary
 //
 //  Created by Andreas Ganske on 01.02.12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Andreas Ganske. All rights reserved.
 //
 
 #import "CCUtils.h"
