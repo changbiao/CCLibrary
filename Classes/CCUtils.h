2af4cfedc229a511ee87b3dc4de0163ca189b0da
Andreas Ganske <a.ganske@chaosspace.de>
2012-02-01 23:50:38 +0100

    Initial commit

---
+++ Classes/CCUtils.h
@@ -0,0 +1,15 @@
+//
+//  CCUtils.h
+//  CCLibrary
+//
+//  Created by Andreas Ganske on 01.02.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define CCLocalized( X ) (NSLocalizedString(X, nil))
+
+@interface CCUtils : NSObject
+
+@end
---
+++ Classes/CCUtils.m
@@ -0,0 +1,13 @@
+//
+//  CCUtils.m
+//  CCLibrary
+//
+//  Created by Andreas Ganske on 01.02.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CCUtils.h"
+
+@implementation CCUtils
+
+@end
