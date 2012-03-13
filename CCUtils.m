d9c91de2da8cc14b4e6e6bcebe30a602a205a8fa
Andreas Ganske <a.ganske@chaosspace.de>
2012-03-13 12:23:13 +0100

    Redone Basics

--- CCUtils.m
+++ CCUtils.m
@@ -7,7 +7,39 @@
 //
 
 #import "CCUtils.h"
+#import <objc/runtime.h>
+
+static NSString *foo = @"foo";
 
 @implementation CCUtils
+
++(CGSize)screenSize:(UIInterfaceOrientation)interfaceOrientation
+{
+switch (interfaceOrientation) {
+	case UIInterfaceOrientationPortrait:
+	case UIInterfaceOrientationPortraitUpsideDown:
+		return CGSizeMake(320, 460);
+		break;
+		
+	case UIInterfaceOrientationLandscapeLeft:
+	case UIInterfaceOrientationLandscapeRight:
+		return CGSizeMake(480, 300);
+		break;
+		
+	default:
+		return CGSizeMake(0, 0);
+		break;
+}
+}
+
++(void)setAssociatedObject:(id)objectA forObject:(id)objectB
+{
+	objc_setAssociatedObject(objectB, &foo, objectA, OBJC_ASSOCIATION_RETAIN);
+}
+
++(id)associatedObjectFor:(id)object
+{
+	return objc_getAssociatedObject(object, &foo);
+}
 
 @end
